d234803e3d2545e581e4b1b91b618b4f6a36b49a
anandgitacc <anand.akmail@gmail.com>
2023-04-15 18:07:13 +0530

    test1

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package org.employee;
+
+
+public class EmployeeDetails {
+	
+	private void empName() {
+		// Business logic
+		System.out.println("Employee name: anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("Employee mobile: 8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeDetails e = new EmployeeDetails();
+		
+		// method call
+		e.empName();
+		e.empMobile();
+		
+	}
+	
+	
+	
+
+}
